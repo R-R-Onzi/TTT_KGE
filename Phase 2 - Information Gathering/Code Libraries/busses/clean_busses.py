1ac11320f363ee6ff689cd41dde35fb125a39a44
Eugenio Ferrari <eugenioferrari81@gmail.com>
2023-10-30 21:02:25 +0100

    Updated clean_busses.py

--- Phase 2 - Information Gathering/Code Libraries/busses/clean_busses.py
+++ Phase 2 - Information Gathering/Code Libraries/busses/clean_busses.py
@@ -1,34 +1,50 @@
 import csv
 from rdflib import RDF, Graph, URIRef
 
+# KGE 2022 - Trentino Urban Transportation
 BUSSES_RDF = './EG RDF.rdf'
 BASE_URI = "http://knowdive.disi.unitn.it/etype#"
 
 g = Graph()
 g.parse(BUSSES_RDF)
 
-
 bus_lines = []
 trips = []
 bus_stops = []
 
 for line in g.subjects(RDF.type, URIRef(BASE_URI + "Line_GID-46379")):
+    line_id = len(bus_lines)
+    line_name = g.value(line, URIRef(BASE_URI + "has_name_GID-34017_Type-132")).toPython()
+    line_long_name = g.value(line, URIRef(BASE_URI + "has_long_name_GID-34017_Type-46379")).toPython()
     line_type = g.value(line, URIRef(BASE_URI + "has_type_GID-25142_Type-46379")).toPython()
 
-    # take only bus transportation
-    if line_type == '3':
-            
-        line_id = len(bus_lines)
-        line_name = g.value(line, URIRef(BASE_URI + "has_name_GID-34017_Type-132")).toPython()
-        line_long_name = g.value(line, URIRef(BASE_URI + "has_long_name_GID-34017_Type-46379")).toPython()
-        
-        bus_lines.append([line_id, f"{line_name} - {line_long_name}"])         
-        
+    # remove lines outside of trento and that are not bus transportation
+    if line_long_name not in [
+        "Funivia Trento Sardagna",
+        "01 Lavis Pressano Nave S.Felice Sorni",
+        "02 Lavis Pressano Masi Sp.131 Sorni",
+        "03 Lavis Stazione Fs",
+        "04 Lavis Zona Industriale Lavis",
+        "Via Magazol S.Giorgio Cornacalda",
+        "Brione Cesari Ospedale Podesta' Brione",
+        "Ossario Osp. S.Giorgio Osp. Ossario",
+        "(Volano) Bettini Staz. Lizzana Marco",
+        "Nomi Villa Lagarina Stazione Mori",
+        "03 Peders. Brione Stazione Isera (Lenzima)",
+        "Costa Corso Rosmini S.Giorgio/Ospedale",
+        "Villa Lag. Brione Staz. Isera Lenzima",
+        "Ospedale Stazione Isera Villa Lagar.",
+        "Volano Besenello Calliano Nomi",
+        "Castellano Molini Villa Lagarina",
+        "Savignano Pomarolo",
+        "Moietto Rosmini Campana Dei Caduti",
+    ] and line_type == '3':
+        bus_lines.append([line_id, f"{line_name} - {line_long_name}"])
+
         for trip in g.objects(line, URIRef(BASE_URI + "has_trips_GID-1501_Type-46379")):            
             trip_calendar = g.value(trip, URIRef(BASE_URI + "has_calendar_GID-80596_Type-1617"))
             
-            calendar_weekdays = g.value(trip_calendar, URIRef(BASE_URI + "has_weekdays_GID-80597_Type-80596")).toPython()
-            # calendar start end day
+            calendar_weekdays = g.value(trip_calendar, URIRef(BASE_URI + "has_weekdays_GID-80597_Type-80596")).toPython()            
             
             trip_schedules = []
             for schedule in g.objects(trip, URIRef(BASE_URI + "has_schedule_GID-34769")):
@@ -38,7 +54,7 @@ for line in g.subjects(RDF.type, URIRef(BASE_URI + "Line_GID-46379")):
                 for stop in g.objects(schedule, URIRef(BASE_URI + "has_stops_GID-5446_Type-34818")):
                     bus_stop_name = g.value(stop, URIRef(BASE_URI + "has_name_GID-34017_Type-132")).toPython()                    
                     bus_long = g.value(stop, URIRef(BASE_URI + "has_longitude_GID-46270_Type-132")).toPython()                    
-                    bus_lat = g.value(stop, URIRef(BASE_URI + "has_latitude_GID-46264_Type-132")).toPython()                    
+                    bus_lat = g.value(stop, URIRef(BASE_URI + "has_latitude_GID-46264_Type-132")).toPython()                                        
 
                     trip_schedules.append([schedule_stop_sequence, schedule_arrival_time, bus_stop_name, float(bus_long), float(bus_lat)])                    
 
@@ -57,9 +73,8 @@ for line in g.subjects(RDF.type, URIRef(BASE_URI + "Line_GID-46379")):
                 bus_stops[bus_stop_id-1].append(bus_stop_id)
             bus_stops[-1].append(None)  # last bus_stop (for the ride/trip)
 
-            # [ trip_id, weekdays, first_bus_stop_id ]
-            trips.append([len(trips), calendar_weekdays, first_bus_stop[0]])
-
+            # [ trip_id, weekdays, bus_line_id, first_bus_stop_id ]
+            trips.append([len(trips), calendar_weekdays, line_id, first_bus_stop[0]])
 
 # save to .tsv, using tabs since I'm not sure that all the names are not containing commas
 
@@ -69,7 +84,7 @@ writer = csv.writer(lines_file, delimiter='\t')
 writer.writerows(bus_lines)
 
 rides_file = open("./bus_rides.txt", 'w')
-rides_file.write("trip_id\tweekdays\tfirst_bus_stop_id\n")
+rides_file.write("trip_id\tweekdays\tbus_line_id\tfirst_bus_stop_id\n")
 writer = csv.writer(rides_file, delimiter='\t')
 writer.writerows(trips)
 
